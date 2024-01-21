f0a8ce344bde2008e49a53d344e1a72073f21e3c
Nikita <nzabrodskii@provenexpert.com>
2024-01-21 20:11:33 +0100

    webpack config set up

--- consumerapp/src/App.js
+++ consumerapp/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+
+const ComponentOne = React.lazy(() => import("firstApp/firstApp"));
+const ComponentTwo = React.lazy(() => import("secondApp/secondApp"));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <React.Suspense fallback="Loading Button">
+        <ComponentOne />
+        <ComponentTwo />
+      </React.Suspense>
     </div>
   );
 }
